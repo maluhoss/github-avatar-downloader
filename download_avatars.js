1c4fecd68a9f5181fe7f62cbcf6acb492499a606
Maliha Hossain <malu.hoss@gmail.com>
2019-03-11 23:09:13 +0000

    added skeleton of get request with get function using request module

--- download_avatars.js
+++ download_avatars.js
@@ -3,8 +3,8 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!')
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
   });
 };
