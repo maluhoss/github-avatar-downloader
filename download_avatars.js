20709f5326ff58766cd7e76fb4377caa345d0c2e
Maliha Hossain <malu.hoss@gmail.com>
2019-03-12 00:02:23 +0000

    removed token

--- download_avatars.js
+++ download_avatars.js
@@ -4,16 +4,16 @@ console.log('Welcome to the GitHub Avatar Downloader!')
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request'
     }
   };
 
-  request(url, function(err, res, body) {
+  request(options, function(err, res, body) {
     cb(err, body);
   });
-};
+  }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
